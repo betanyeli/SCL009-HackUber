7508712ca0d7a82506116c0249a0dc01574bd578
betanyeli <betangelii@gmail.com>
2019-10-02 16:45:27 -0300

    adding new boilerplate && deployment with firebase

---
+++ build/precache-manifest.fc98561462e5bdbbdd8235c2b7f7f1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d204638c4e63a414befba5e1ebeb05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5575872f55ba5d9e057b",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "45c2ba614f38d0233e89",
+    "url": "/static/js/2.2e2dd5bc.chunk.js"
+  },
+  {
+    "revision": "5575872f55ba5d9e057b",
+    "url": "/static/js/main.ef607f23.chunk.js"
+  },
+  {
+    "revision": "31c8009a99be529a3550",
+    "url": "/static/js/runtime-main.4845cfe7.js"
+  }
+]);
